49ec8c287288ce15772b00f1011c011ca66ae874
yayars <yasmin.silva2@aluno.ifsp.edu.br>
2026-08-10 17:47:33 -0300

    lgcc

--- src/App.js
+++ src/App.js
@@ -5,60 +5,62 @@ export default function App() {
   return (
     <View style={styles.container}>
 
+      <View style={styles.caixa}>
 
-      <View style={styles.header}>
-        <Image
-          style={styles.logo}
-           source={'https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456'}
-        />
-        <Text style={styles.titulo}>IFFlix</Text>
+        <View style={styles.header}>
+          <Image
+            style={styles.logo}
+            source={'https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456'}
+          />
+          <Text style={styles.titulo}>IFFlix</Text>
+        </View>
+
+
+        <View style={styles.linha}>
+          <View style={styles.card}>
+            <Image
+              style={styles.imagem}
+              source={'https://m.media-amazon.com/images/S/pv-target-images/2745382833c26f576e1c63466a8cb9ee8ff1f30d3cc05e90c009d014fa0b07bc.jpg'}
+            />
+            <Text style={styles.nome}>O Mentalista</Text>
+            <Text style={styles.plataforma}>Netflix</Text>
+          </View>
+
+
+          <View style={styles.card}>
+            <Image
+              style={styles.imagem}
+              source={'https://daniquedisse.com.br/wp-content/uploads/2017/03/antes-e-depois-one-tree-hill.png'}
+            />
+            <Text style={styles.nome}>One Tree Hill</Text>
+            <Text style={styles.plataforma}>Netflix</Text>
+          </View>
+        </View>
+
+
+        <View style={styles.linha}>
+          <View style={styles.card}>
+            <Image
+              style={styles.imagem}
+              source={'https://mundonegro.inf.br/wp-content/uploads/2025/03/Copia-de-Copia-de-CAPA-MN-2022-2025-03-12T161209.170-768x432.jpg'}
+            />
+            <Text style={styles.nome}>Beauty in Black</Text>
+            <Text style={styles.plataforma}>Netflix</Text>
+          </View>
+
+
+          <View style={styles.card}>
+            <Image
+              style={styles.imagem}
+              source={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSorCl3HXTc--xAjoRVeLFcrAFK6WCR24-jAbWBAIH6Ycsxa4CSqqLc2n5U&s=10'}
+            />
+            <Text style={styles.nome}>Breaking Bad</Text>
+            <Text style={styles.plataforma}>Netflix</Text>
+          </View>
+        </View>
+
+        <StatusBar style="auto" />
       </View>
-
-
-      <View style={styles.linha}>
-        <View style={styles.card}>
-          <Image
-            style={styles.imagem}
-            source={'https://m.media-amazon.com/images/S/pv-target-images/2745382833c26f576e1c63466a8cb9ee8ff1f30d3cc05e90c009d014fa0b07bc.jpg'}
-          />
-          <Text style={styles.nome}>O Mentalista</Text>
-          <Text style={styles.plataforma}>Netflix</Text>
-        </View>
-
-
-        <View style={styles.card}>
-          <Image
-            style={styles.imagem}
-            source={'https://daniquedisse.com.br/wp-content/uploads/2017/03/antes-e-depois-one-tree-hill.png'}
-          />
-          <Text style={styles.nome}>One Tree Hill</Text>
-          <Text style={styles.plataforma}>Netflix</Text>
-        </View>
-      </View>
-
-
-      <View style={styles.linha}>
-        <View style={styles.card}>
-          <Image
-            style={styles.imagem}
-            source={'https://mundonegro.inf.br/wp-content/uploads/2025/03/Copia-de-Copia-de-CAPA-MN-2022-2025-03-12T161209.170-768x432.jpg'}
-          />
-          <Text style={styles.nome}>Beauty in Black</Text>
-          <Text style={styles.plataforma}>Netflix</Text>
-        </View>
-
-
-        <View style={styles.card}>
-          <Image
-            style={styles.imagem}
-            source={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSorCl3HXTc--xAjoRVeLFcrAFK6WCR24-jAbWBAIH6Ycsxa4CSqqLc2n5U&s=10'}
-          />
-          <Text style={styles.nome}>Breaking Bad</Text>
-          <Text style={styles.plataforma}>Netflix</Text>
-        </View>
-      </View>
-
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -67,9 +69,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 60,
+    paddingTop: 80,
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+
+  caixa: {
+    backgroundColor: '#a7242a',
+    padding: 50,
+    alignItems: 'center',
   },
 
   header: {
@@ -92,19 +100,18 @@ const styles = StyleSheet.create({
 
   linha: {
     flexDirection: 'row',
-    marginBottom: 20,
   },
 
   card: {
     alignItems: 'center',
-    marginHorizontal: 10,
+    marginHorizontal: 20,
+    marginVertical: 10,
   },
 
   imagem: {
-    width: 120,
-    height: 120,
+    width: 140,
+    height: 180,
     borderRadius: 15,
-    borderWidth: 2,
   },
 
   nome: {
@@ -114,7 +121,7 @@ const styles = StyleSheet.create({
   },
 
   plataforma: {
-    color: 'gray',
+    color: 'white',
     fontSize: 12,
   },
 });
